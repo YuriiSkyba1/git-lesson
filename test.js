3a3c497d14251073382e99bbf1e0a438c7ce690c
YuriiSkyba1 <yuraaa1111@gmail.com>
2024-01-17 21:10:48 +0200

    test1

--- test.js
+++ test.js
@@ -94,5 +94,60 @@ function a(){
 
 }console.log()
 function a(){
+console.log()
+console.log()
+console.log()
+
+function b(){
+console.log()
+console.log()
+
+dddddd
+}console.logdd
+function b(){
+    function a(){
+        dddddd
+        }
+}console.log()
+function b(){
+
+}console.log()
+function b(){
+
+}console.log()
+function b(){
+
+}console.log()
+function b(){
+
+}console.log()
+function b(){
+
+}console.log()
+function b(){
+
+}console.log()
+function b(){
+
+}console.log()
+function b(){
+
+}console.log()
+function b(){
+
+}console.log()
+function b(){
+
+}console.log()
+function b(){
+
+}console.log()
+function b(){
+dddddd
+}console.log()
+function b(){
+
+}console.log()
+function b(){
 
 }
